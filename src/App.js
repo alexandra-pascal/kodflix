f87a4cc502c729888598470f516a8cc211b89f96
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-11-01 11:08:14 +0000

    added an image

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import venom from './img/venom.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Kodflix</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           Did it!
-        </p>
+        </p> */}
+        <header className="App-body">
+          <img src={venom} className="App-img" alt="img" />
+        </header>
       </div>
     );
   }
