5b966cb0e9ce820d068c7bde3900b36d5a21b722
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-11-01 15:24:06 +0000

    Made the movie title appear when hovering-over the image #5

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import venom from './img/venom.jpg';
 import man from './img/man.jpg';
 import paddington from './img/paddington.jpg';
 import nutcracker from './img/nutcracker.jpg';
-import runner from './img/runner.jpeg';
+import runner from './img/runner.jpg';
 import thor from './img/thor.jpg';
-import wonder from './img/wonder.jpeg';
+import wonder from './img/wonder.jpg';
 import './App.css';
 
 class App extends Component {
@@ -16,24 +16,42 @@ class App extends Component {
           <div className="flex-grid">
             <div className="wrap-cover">
               <img src={man} className="img" alt="first-man-cover" />
+              <div className="wrap-text">
+                <h2 className="App-title">First Man</h2>
+              </div>
             </div>
             <div className="wrap-cover">
               <img src={paddington} className="img" alt="paddington2-cover" />
+              <div className="wrap-text">
+                <h2 className="App-title">Paddington 2</h2>
+              </div>
             </div>
             <div className="wrap-cover">
               <img src={nutcracker} className="img" alt="nutcracker-cover" />
+              <div className="wrap-text">
+                <h2 className="App-title">Nutcracker</h2>
+              </div>
             </div>
           </div>
 
           <div className="flex-grid">
             <div className="wrap-cover">
               <img src={runner} className="img" alt="blade-runner-cover" />
+              <div className="wrap-text">
+                <h2 className="App-title">Blade Runner</h2>
+              </div>
             </div>
             <div className="wrap-cover">
               <img src={thor} className="img" alt="thor-cover" />
+              <div className="wrap-text">
+                <h2 className="App-title">Thor Ragnarok</h2>
+              </div>
             </div>
             <div className="wrap-cover">
               <img src={wonder} className="img" alt="wonder-woman-cover" />
+              <div className="wrap-text">
+                <h2 className="App-title">Wonder Woman</h2>
+              </div>
             </div>
           </div>
       </div>
