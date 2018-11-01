cb3a2a8933151eafef2eb6feb926e5b0355b8461
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-11-01 14:05:03 +0000

    Replaced the titles for the covers with images of the movies #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import venom from './img/venom.jpg';
+import man from './img/man.jpg';
+import paddington from './img/paddington.jpg';
+import nutcracker from './img/nutcracker.jpg';
+import runner from './img/runner.jpeg';
+import thor from './img/thor.jpg';
+import wonder from './img/wonder.jpeg';
 import './App.css';
 
 class App extends Component {
@@ -9,25 +15,25 @@ class App extends Component {
        <img src={venom} className="venom-cover" alt="venom-cover" />
           <div className="flex-grid">
             <div className="wrap-cover">
-              <h2 className="App-title">Venom</h2>
+              <img src={man} className="img" alt="first-man-cover" />
             </div>
             <div className="wrap-cover">
-              <h2 className="App-title">Wonder Woman</h2>
+              <img src={paddington} className="img" alt="paddington2-cover" />
             </div>
             <div className="wrap-cover">
-              <h2 className="App-title">Nutcracker</h2>
+              <img src={nutcracker} className="img" alt="nutcracker-cover" />
             </div>
           </div>
 
           <div className="flex-grid">
             <div className="wrap-cover">
-              <h2 className="App-title">Blade Runner</h2>
+              <img src={runner} className="img" alt="blade-runner-cover" />
             </div>
             <div className="wrap-cover">
-              <h2 className="App-title">Thor Ragnarok</h2>
+              <img src={thor} className="img" alt="thor-cover" />
             </div>
             <div className="wrap-cover">
-              <h2 className="App-title">Paddington 2</h2>
+              <img src={wonder} className="img" alt="wonder-woman-cover" />
             </div>
           </div>
       </div>
