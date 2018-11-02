f236d9f542c50f6fa8b8bac3fb1333763223ee2c
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-11-02 12:40:11 +0000

    Transformed the movie cover element into a single component #7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import venom from './img/venom.jpg';
 import man from './img/man.jpg';
 import paddington from './img/paddington.jpg';
 import nutcracker from './img/nutcracker.jpg';
@@ -12,51 +11,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <img src={venom} className="venom-cover" alt="venom-cover" />
           <div className="flex-grid">
-            <div className="wrap-cover">
-              <img src={man} className="img" alt="first-man-cover" />
-              <div className="wrap-text">
-                <h2 className="App-title">First Man</h2>
-              </div>
-            </div>
-            <div className="wrap-cover">
-              <img src={paddington} className="img" alt="paddington2-cover" />
-              <div className="wrap-text">
-                <h2 className="App-title">Paddington 2</h2>
-              </div>
-            </div>
-            <div className="wrap-cover">
-              <img src={nutcracker} className="img" alt="nutcracker-cover" />
-              <div className="wrap-text">
-                <h2 className="App-title">Nutcracker</h2>
-              </div>
-            </div>
+            <CoverPoster title="First Man" image={man}/>
+            <CoverPoster title="Paddington" image={paddington}/>
+            <CoverPoster title="Nutcracker" image={nutcracker}/>
           </div>
-
           <div className="flex-grid">
-            <div className="wrap-cover">
-              <img src={runner} className="img" alt="blade-runner-cover" />
-              <div className="wrap-text">
-                <h2 className="App-title">Blade Runner</h2>
-              </div>
-            </div>
-            <div className="wrap-cover">
-              <img src={thor} className="img" alt="thor-cover" />
-              <div className="wrap-text">
-                <h2 className="App-title">Thor Ragnarok</h2>
-              </div>
-            </div>
-            <div className="wrap-cover">
-              <img src={wonder} className="img" alt="wonder-woman-cover" />
-              <div className="wrap-text">
-                <h2 className="App-title">Wonder Woman</h2>
-              </div>
-            </div>
+            <CoverPoster title="Blade Runner" image={runner}/>
+            <CoverPoster title="Thor Ragnarok" image={thor}/>
+            <CoverPoster title="Wonder Woman" image={wonder}/>
           </div>
       </div>
     );
   }
 }
-
+const CoverPoster = (props) => {
+  return (
+    <div className="wrap-cover">
+      <img src={props.image} className="image" alt={props.title} />
+      <h2>{props.title}</h2>
+    </div>
+  );
+};
 export default App;
