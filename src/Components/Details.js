ca2a72324c10b231322f5d1fb795ae0b401e163f
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-11-26 14:05:54 +0000

    Created a stateful component in the Details page and changed automatically the title with the setTimeout method

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/details.css';
 
-const Details = () => {
-  return (
-    <div>
-      <h1>Hello, this will be the details page for each Movie and TV Show</h1>
-    </div>
-  );
-}
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = {message: "Hello, this will be the details page for each Movie and TV Show"}
+  }
 
+  componentDidMount() {
+    setTimeout(()=> {
+      this.setState({
+        message: "Coming soon! :)"
+      });
+    }, 3000)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+      </div>
+    )
+  }
+}
 export default Details;
